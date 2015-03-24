0bcd3922fde6842ef4ef2181df33eaed5e3b5f71
seadog007 <007seadog@gmail.com>
2015-03-25 00:22:30 +0800

    Can Use Two Proxy In Same Time

--- do_until_no_ads.sh
+++ do_until_no_ads.sh
@@ -2,8 +2,21 @@ self=$1
 ref='1N38jUyZhrbRPJrLKJHQ2aNVLKA4HmYSJW'
 islocal=$2
 proxy=$3
+proxy2=$4
+line=$5
 
-until [ $? -gt 1 ]
+laststatus=0
+until [ $laststatus -gt 1 ]
 do
 	./send.sh $self $ref $islocal $proxy
+  laststatus=$?
 done
+[ $laststatus -ge 17 ] || [ $laststatus -ge 31 ] && echo "$line Need to Change the proxy1"
+
+laststatus=0
+until [ $laststatus -gt 1 ]
+do
+  ./send.sh $self $ref $islocal $proxy2
+  laststatus=$?
+done
+[ $laststatus -ge 17 ] || [ $laststatus -ge 31 ] && echo "$line Need to Change the proxy2"
