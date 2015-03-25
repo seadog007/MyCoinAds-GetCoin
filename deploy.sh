d1b124cad73cbcbf5b55d59767b07b666c415397
seadog007 <007seadog@gmail.com>
2015-03-25 21:32:53 +0800

    Fixed Death Thread

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # clear
+limit=100
 while read line
 do
 	set -- "$line"
   IFS=";"; declare -a Array=($*)
+  echo `jobs | wc -l`
+  until [ `jobs | wc -l | sed 's/ //g'` -lt $limit ]
+  do
+    sleep 0.2
+  done
   [ -z "${Array[2]}" ] && ./do_until_no_ads.sh "${Array[1]}" 1 &
   [ -n "${Array[2]}" ] && [ -n "${Array[3]}" ] && ./do_until_no_ads.sh "${Array[1]}" 0 "${Array[2]}" "${Array[3]}" &
 done < mergelist
--- do_until_no_ads.sh
+++ do_until_no_ads.sh
@@ -3,7 +3,6 @@ ref='1N38jUyZhrbRPJrLKJHQ2aNVLKA4HmYSJW'
 islocal=$2
 proxy=$3
 proxy2=$4
-line=$5
 
 laststatus=0
 until [ $laststatus -gt 0 ]
@@ -11,7 +10,7 @@ do
 	./send.sh $self $ref $islocal $proxy
   laststatus=$?
 done
-[ $laststatus -ge 17 ] || [ $laststatus -ge 31 ] && echo "$line Need to Change the proxy1"
+[ $laststatus -ge 17 ] || [ $laststatus -ge 31 ] && echo "$self Need to Change the proxy1"
 
 laststatus=0
 until [ $laststatus -gt 0 ]
@@ -19,4 +18,4 @@ do
   ./send.sh $self $ref $islocal $proxy2
   laststatus=$?
 done
-[ $laststatus -ge 17 ] || [ $laststatus -ge 31 ] && echo "$line Need to Change the proxy2"
+[ $laststatus -ge 17 ] || [ $laststatus -ge 31 ] && echo "$self Need to Change the proxy2"
