b32c527e63871818378ada123ac42c2d0e0f5454
seadog007 <007seadog@gmail.com>
2015-03-27 14:11:31 +0800

    Fixed Damn it Problem = = & Re-gen. Merge List

--- mergelist.sh
+++ mergelist.sh
@@ -3,6 +3,6 @@ rm mergelist
 i=1
 while read line
 do
-  echo "$line;`sed -n "$i"p addrlist`;`sed -n "$i"p proxylist`;`sed -n "$i"p proxylist2`" >> mergelist
+  echo "$line;`sed -n "$i"p proxylist`;`sed -n "$i"p proxylist2`" >> mergelist
   i=$((i + 1))
 done < addrlist
