8c5893ff639a4b04b3e87e849d00d0b09a374db6
seadog007 <007seadog@gmail.com>
2015-03-28 16:59:55 +0800

    Update All Proxy List & Change Update Proxy Function

--- update_proxy.sh
+++ update_proxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cp mergelist mergelist.original
 clear
 line_num=0
 while read line
@@ -8,6 +8,6 @@ do
 	set -- "$line"
 	IFS=" "; declare -a Array=($*)
 	cat mergelist | sed "s/${Array[0]}.*/${Array[0]};`sed -n "$line_num"p proxylist`;`sed -n "$line_num"p proxylist2`/" > mergelist.x
-	#rm mergelist
-	#mv mergelist.x mergelist
+	rm mergelist
+	mv mergelist.x mergelist
 done < <(cat log.log | sort | uniq)
