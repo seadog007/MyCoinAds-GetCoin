1f47e304ed2f1f0267a4e755851eeecad3809fd9
seadog007 <007seadog@gmail.com>
2015-03-30 18:56:10 +0800

    Added All Function

--- total.sh
+++ total.sh
@@ -7,6 +7,6 @@ do
   this="`curl -s "http://mycoinads.com/showstats.php?btcaddress="${Array[0]}" " -H "Cookie: btcaddress="${Array[0]}" ;" | grep 'Total Earned:' | sed 's/<.[^>]*>//g' | sed 's/Total Earned//' | sed 's/ BTC//g' | sed 's/: //g'`"
   #[ `echo "$this > 0" | bc` -eq 1 ] && echo $this
   [ `echo "$this > 0" | bc` -eq 1 ] && total=`echo "$this + $total" | bc`
-  [ `echo "$this > 0" | bc` -eq 1 ] && echo $total
+  [ `echo "$this > 0" | bc` -eq 1 ] && echo "$line : $total"
 done < mergelist
 sleep 3
