1553af45b20ec35c224d82246371efcaecc55191
seadog007 <007seadog@gmail.com>
2015-04-13 22:07:30 +0800

    Fixed total & added echo limit

--- echo.sh
+++ echo.sh
@@ -1,11 +1,14 @@
+echo "Please enter limit:"
+read limit
 while true
 do
- 	clear
+	clear
 	while read line
 	do
 		set -- "$line"
 		IFS=";"; declare -a Array=($*)
-		echo "`date`   "${Array[0]}"`curl -s "http://mycoinads.com/showstats.php?btcaddress="${Array[0]}" " -H "Cookie: btcaddress="${Array[0]}" ;" | grep 'Total Earned:' | sed 's/<.[^>]*>//g' | sed 's/Total Earned//'`"
-	done < mergelist
+		this=`curl -s "http://mycoinads.com/showstats.php?btcaddress="${Array[0]}" " -H "Cookie: btcaddress="${Array[0]}" ;" | grep 'Total Earned:' | sed 's/<.[^>]*>//g' | sed 's/Total Earned//' | sed 's/ BTC//g' | sed 's/: //g'`
+		[ `echo "$this > $limit" | bc` -eq 1 ] && echo "${Array[0]}\t$this"
+	done < ./list/mergelist
 	sleep 3
 done
