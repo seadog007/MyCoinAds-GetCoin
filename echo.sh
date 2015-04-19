f5338e537189f75e54d9196fcb8c5443a338188f
seadog007 <007seadog@gmail.com>
2015-04-19 14:36:33 +0800

    Fixed echo and total function

--- echo.sh
+++ echo.sh
@@ -8,7 +8,7 @@ do
 		set -- "$line"
 		IFS=";"; declare -a Array=($*)
 		this=`curl -s "http://mycoinads.com/showstats.php?btcaddress="${Array[0]}" " -H "Cookie: btcaddress="${Array[0]}" ;" | grep 'Total Earned:' | sed 's/<.[^>]*>//g' | sed 's/Total Earned//' | sed 's/ BTC//g' | sed 's/: //g'`
-		[ `echo "$this > $limit" | bc` -eq 1 ] && echo "${Array[0]}\t$this"
+		[ `echo "$this > $limit" | bc` -eq 1 ] && echo -e "${Array[0]}\t$this"
 	done < ./list/mergelist
 	sleep 3
 done
--- total.sh
+++ total.sh
@@ -7,6 +7,6 @@ do
   this="`curl -s "http://mycoinads.com/showstats.php?btcaddress="${Array[0]}" " -H "Cookie: btcaddress="${Array[0]}" ;" | grep 'Total Earned:' | sed 's/<.[^>]*>//g' | sed 's/Total Earned//' | sed 's/ BTC//g' | sed 's/: //g' | sed 's/ BTC//g' | sed 's/: //g'`"
   #[ `echo "$this > 0" | bc` -eq 1 ] && echo $this
   [ `echo "$this > 0" | bc` -eq 1 ] && total=`echo "$this + $total" | bc`
-  [ `echo "$this > 0" | bc` -eq 1 ] && echo "${Array[0]}\t$total"
+  [ `echo "$this > 0" | bc` -eq 1 ] && echo -e "${Array[0]}\t$total"
 done < ./list/mergelist
 sleep 3
