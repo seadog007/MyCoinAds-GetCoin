3f0a369a47848e6052156f0a3c91c279514bba2b
Japer Yu <007seadog@gmail.com>
2015-04-25 03:29:02 +0800

    Update count function

--- received.sh
+++ received.sh
@@ -7,11 +7,8 @@ do
   res="`curl -s "http://mycoinads.com/showstats.php?btcaddress="${Array[0]}" " -H "Cookie: btcaddress="${Array[0]}" ;"`"
   tt="`echo "$res" | grep 'Total Earned:' | sed 's/<.[^>]*>//g' | sed 's/Total Earned: //' | sed 's/ BTC//g'`"
   tb=`echo "$res" | grep 'Your Current Balance:' | sed 's/<.[^>]*>//g' | sed 's/Your Current Balance: //g' | sed 's/ BTC//g'`
-  echo $res
-  echo $tt
-  echo $tb
-  #[ `echo "$this > 0" | bc` -eq 1 ] && echo $this
-  #[ `echo "$this > 0" | bc` -eq 1 ] && total=`echo "$this + $total" | bc`
-  #[ `echo "$this > 0" | bc` -eq 1 ] && echo -e "${Array[0]}\t$total"
+  this=`echo "$tt - $tb" | bc`
+  [ `echo "$this > 0" | bc` -eq 1 ] && total=`echo "$this + $total" | bc`
+  [ `echo "$this > 0" | bc` -eq 1 ] && echo -e "${Array[0]}\t$total"
 done < ./list/mergelist
 sleep 3
--- total.sh
+++ total.sh
@@ -1,12 +1,15 @@
-total=0
-clear
-while read line
+while true
 do
-  set -- "$line"
-  IFS=";"; declare -a Array=($*)
-  this="`curl -s "http://mycoinads.com/showstats.php?btcaddress="${Array[0]}" " -H "Cookie: btcaddress="${Array[0]}" ;" | grep 'Total Earned:' | sed 's/<.[^>]*>//g' | sed 's/Total Earned//' | sed 's/ BTC//g' | sed 's/: //g' | sed 's/ BTC//g' | sed 's/: //g'`"
-  #[ `echo "$this > 0" | bc` -eq 1 ] && echo $this
-  [ `echo "$this > 0" | bc` -eq 1 ] && total=`echo "$this + $total" | bc`
-  [ `echo "$this > 0" | bc` -eq 1 ] && echo -e "${Array[0]}\t$total"
-done < ./list/mergelist
-sleep 3
+	total=0
+	clear
+	while read line
+	do
+		set -- "$line"
+		IFS=";"; declare -a Array=($*)
+		this="`curl -s "http://mycoinads.com/showstats.php?btcaddress="${Array[0]}" " -H "Cookie: btcaddress="${Array[0]}" ;" | grep 'Total Earned:' | sed 's/<.[^>]*>//g' | sed 's/Total Earned//' | sed 's/ BTC//g' | sed 's/: //g' | sed 's/ BTC//g' | sed 's/: //g'`"
+		#[ `echo "$this > 0" | bc` -eq 1 ] && echo $this
+		[ `echo "$this > 0" | bc` -eq 1 ] && total=`echo "$this + $total" | bc`
+		[ `echo "$this > 0" | bc` -eq 1 ] && echo -e "${Array[0]}\t$total"
+	done < ./list/mergelist
+	echo $total >> ./logs/total
+done
