0d3486e079747958e0ae7ae029899503ec1c28b8
seadog007 <007seadog@gmail.com>
2015-04-25 03:30:57 +0800

    Update all data

--- get_proxy_list.sh
+++ get_proxy_list.sh
@@ -1,4 +1,4 @@
 ./proxy_lib/nntime.sh >> /dev/null
-./proxu_lib/hidemyass.py >> /dev/null
+./proxy_lib/hidemyass.py >> ./list/proxylist
 cat ./list/proxylist | sort -R > ./list/proxylist2
 cat ./list/proxylist2 | sort -R > ./list/proxylist
