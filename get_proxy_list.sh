f312fc799f8f680a417f0903f39f841119dd9f55
Japer Yu <007seadog@gmail.com>
2015-06-08 16:15:08 +0800

    Fixed IPv6 Problem

--- get_proxy_list.sh
+++ get_proxy_list.sh
@@ -1,3 +1,3 @@
-curl -s http://srv.seadog007.me:9182/proxylist > ./list/proxylist
+curl --ipv4 -s http://srv.seadog007.me:9182/proxylist -O ./list/proxylist
 cat ./list/proxylist | uniq | sort -R > ./list/proxylist2
 cat ./list/proxylist2 | sort -R > ./list/proxylist
